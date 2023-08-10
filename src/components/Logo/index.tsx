b11894f220db58542eb664cc6f991045a3131d1d
Arunkumar Selvaraj <arun@ioak.io>
2023-08-10 09:47:31 +0530

    base version

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, connect, useDispatch } from 'react-redux';
+import './style.scss';
+import winmeetWhiteSmall from '../../images/winmeet_white_small.svg';
+import winmeetWhiteText from '../../images/winmeet_white_text.svg';
+import winmeetBlackSmall from '../../images/winmeet_black_small.svg';
+import winmeetBlackText from '../../images/winmeet_black_text.svg';
+import winmeetBlack from '../../images/winmeet_black.svg';
+
+interface Props {
+  variant: 'full' | 'short';
+}
+
+const Logo = (props: Props) => {
+  const authorization = useSelector((state: any) => state.authorization);
+
+  const profile = useSelector((state: any) => state.profile);
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="logo">
+      <div className="logo--image">
+        {profile.theme === 'basicui-light' && (
+          <img src={winmeetWhiteSmall} alt="Winmeet logo" />
+        )}
+        {profile.theme !== 'basicui-light' && (
+          <img src={winmeetWhiteSmall} alt="Winmeet logo" />
+        )}
+      </div>
+      {props.variant === 'full' && (
+        <div className="logo--text">
+          <img src={winmeetWhiteText} alt="Winmeet logo" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/constants/SessionStorageConstants.ts
@@ -0,0 +1,4 @@
+export const WINMEET_PREF_ADDEXPENSE_ANOTHER =
+  'winmeet_pref_addexpense_another';
+export const WINMEET_PREF_ADDBILL_ANOTHER = 'winmeet_pref_addbill_another';
+export const WINMEET_PREF_ADDBILL_DATE = 'winmeet_pref_addbill_date';
